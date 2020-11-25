76adad1a66b3e4f19839fd46ef4f53bb1cd1336f
六号加油机 <1487238937@qq.com>
2020-11-25 17:33:01 +0800

    默认

--- src/_20201118/_9_8/NoIpException.java
+++
@@ -1,7 +0,0 @@
-package _20201118._9_8;
-
-public class NoIpException extends Exception{
-    public NoIpException(String message) {
-        super(message);
-    }
-}
--- src/_20201122/_10_1/Demo01_1.java
+++
@@ -1,9 +0,0 @@
-package _20201122._10_1;
-
-public class Demo01_1 {
-    public static void main(String[] args) {
-        Persion persion1 = new Persion("2016001","张三");
-        Persion persion2 = new Persion("2016001","小三");
-        System.out.println(persion1.equals(persion2));
-    }
-}
