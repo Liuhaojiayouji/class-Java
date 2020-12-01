cd071612133e9ec2a2fa9a588bf8586065316be4
六号加油机 <1487238937@qq.com>
2020-12-01 22:56:48 +0800

    作业

---
+++ src/_20201201/_10_5/Demo17.java
@@ -0,0 +1,11 @@
+package _20201201._10_5;
+
+public class Demo17 {
+    public static void main(String[] args) {
+        Integer num1 = new Integer(10);
+        Double d1 = new Double(20.11);
+        Integer d2 = Integer.valueOf(20);
+        int num3 = num1.intValue();
+        double d3 = d2.doubleValue();
+    }
+}
---
+++ src/_20201201/_10_5/Demo17_2.java
@@ -0,0 +1,10 @@
+package _20201201._10_5;
+
+public class Demo17_2 {
+    public static void main(String[] args) {
+        int num1 = 20;
+        Integer num2 = 30;
+        int num3 = num1 + num2;
+        System.out.println(num3);
+    }
+}
